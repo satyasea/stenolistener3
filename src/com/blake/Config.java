2885908c1bad37351c2671248221212ac6d431d3
gitgong <saltyfog@gmail.com>
2014-08-24 12:36:56 -0700

    new config file

---
+++ src/com/blake/Config.java
@@ -0,0 +1,10 @@
+package com.blake;
+
+/**
+ * Created by horse on 8/24/2014.
+ */
+public class Config {
+    public static boolean IS_AEH_BUILD;
+    public static boolean IS_MULTIUSER;
+    public static boolean IS_DEMO;
+}
